84fc400ce01a027c1517e8398ffae9e094cd9da9
陈伟鹏 <981208572@qq.com>
2025-05-22 15:54:07 +0800

    feat:整理git忽略文件

--- FantasyServer/Server/Entity/Generate/NetworkProtocol/RouteType.cs
+++
@@ -1,9 +0,0 @@
-namespace Fantasy
-{
-	// Route协议定义(需要定义1000以上、因为1000以内的框架预留)	
-	public static class RouteType
-	{
-		public const int GateRoute = 1001; // Gate
-		public const int ChatRoute = 1002; // Chat
-	}
-}
--- UnityProject/Assets/GameScripts/HotFix/GameLogic/Module/NetModule/NetModule.cs
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/Module/NetModule/NetModule.cs
@@ -16,6 +16,22 @@ namespace GameLogic
         {
             await Entry.Initialize();
             NetScene = await Entry.CreateScene();
+            NetScene.Connect("127.0.0.1:20000", Fantasy.Network.NetworkProtocolType.KCP, OnConnectSuccessResponse, OnConnectFailResponse, OnConnectDisConnectResponse, false);
+        }
+
+        private void OnConnectSuccessResponse()
+        {
+            Log.Debug("Fantasy 链接成功");
+        }
+
+        private void OnConnectFailResponse()
+        {
+            Log.Debug("Fantasy 链接失败");
+        }
+
+        private void OnConnectDisConnectResponse()
+        {
+            Log.Debug("Fantasy 断开链接");
         }
 
         public void OnUpdate()
